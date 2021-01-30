ca4ac3c58b3c64b881a42a4e6fec21c125dd25f8
Matthew Peveler <matt.peveler@gmail.com>
2021-01-30 01:59:35 +0000

    disable calling asciidoc package without -m flag
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- asciidoc/__main__.py
+++ asciidoc/__main__.py
@@ -1,7 +1,7 @@
 try:
     from .asciidoc import cli, set_caller
 except ImportError:
-    from asciidoc import cli, set_caller
+    raise SystemExit('ERROR: You must execute as a module using the -m flag')
 
 set_caller(__name__)
 cli()
