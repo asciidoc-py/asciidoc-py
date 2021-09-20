8336c4882a7f01f82b69512eb121948ed719c9bc
Matthew Peveler <matt.peveler@gmail.com>
2021-09-19 19:03:10 -1000

    test userdir

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,67 +1,82 @@
 import pytest
+from pytest_mock import MockerFixture
+from typing import Optional
+
 from asciidoc import utils
 
 
 @pytest.mark.parametrize(
     "input,expected",
-    [
+    (
+        ('/home/user', '/home/user'),
+        ('~', None),
+    )
+)
+def test_userdir(mocker: MockerFixture, input: str, expected: Optional[str]) -> None:
+    mocker.patch('os.path.expanduser', return_value=input)
+    assert utils.userdir() == expected
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    (
         (' a ', 'a'),
         ('"a"', 'a'),
         ('  "b ', '"b'),
         ('  b" ', 'b"'),
         ('""', '""'),
-    ],
+    ),
 )
-def test_strip_quotes(input, expected):
+def test_strip_quotes(input: str, expected: str) -> None:
     assert utils.strip_quotes(input) == expected
 
 
 @pytest.mark.parametrize(
     "input,expected",
-    [
+    (
         (('a', 'b'), ('a', 'b')),
         (('', 'a', 'b'), ('a', 'b')),
         (('a', 'b', ''), ('a', 'b', '')),
         (('', 'a', 'b', ''), ('a', 'b', '')),
-    ],
+    ),
 )
-def test_lstrip_list(input, expected):
+def test_lstrip_list(input: tuple[str, ...], expected: tuple[str, ...]) -> None:
     assert utils.lstrip_list(input) == expected
 
 
 @pytest.mark.parametrize(
     "input,expected",
-    [
+    (
         (('a', 'b'), ('a', 'b')),
         (('', 'a', 'b'), ('', 'a', 'b')),
         (('a', 'b', ''), ('a', 'b')),
         (('', 'a', 'b', ''), ('', 'a', 'b')),
-    ],
+    ),
 )
-def test_rstrip_list(input, expected):
+def test_rstrip_list(input: tuple[str, ...], expected: tuple[str, ...]) -> None:
     assert utils.rstrip_list(input) == expected
 
 
 @pytest.mark.parametrize(
     "input,expected",
-    [
+    (
         (('a', 'b'), ('a', 'b')),
         (('', 'a', 'b'), ('a', 'b')),
         (('a', 'b', ''), ('a', 'b')),
         (('', 'a', 'b', ''), ('a', 'b')),
-    ],
+    ),
 )
-def test_strip_list(input, expected):
+def test_strip_list(input: tuple[str, ...], expected: tuple[str, ...]) -> None:
     assert utils.strip_list(input) == expected
 
 
 @pytest.mark.parametrize(
     "input,expected",
-    [
+    (
         ((1,), True),
         ([1], True),
         ('a', False),
-    ],
+    ),
 )
 def test_is_array(input, expected):
     assert utils.is_array(input) == expected
@@ -69,7 +84,7 @@ def test_is_array(input, expected):
 
 @pytest.mark.parametrize(
     "n,d,expected",
-    [
+    (
         (42.0, 0, 42),
         (42.4, 0, 42),
         (42.5, 0, 43),
@@ -82,7 +97,7 @@ def test_is_array(input, expected):
         (42.555, 2, 42.56),
         (42.556, 2, 42.56),
         (42.559, 2, 42.56),
-    ],
+    ),
 )
-def test_py2round(n, d, expected):
+def test_py2round(n: float, d: int, expected: float) -> None:
     assert utils.py2round(n, d) == expected
