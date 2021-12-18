ace8de363b031f9fa31657bff9017c2afd60938b
Matthew Peveler <matt.peveler@gmail.com>
2021-12-18 10:50:46 -0500

    Fix RuntimeWarning when using the -m flag
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- asciidoc/__init__.py
+++ asciidoc/__init__.py
@@ -1,7 +1,13 @@
 """asciidoc module"""
 
-from .api import AsciiDocAPI
-from .asciidoc import execute, cli
+import sys
 from .__metadata__ import VERSION, __version__
 
-__all__ = ['AsciiDocAPI', 'execute', 'cli', 'VERSION', '__version__']
+__all__ = ['VERSION', '__version__']
+
+# If running as a script, we avoid these imports to avoid a circular
+# RuntimeWarning, which is fine as we don't use them in that case.
+if "-m" not in sys.argv:
+    from .api import AsciiDocAPI
+    from .asciidoc import execute, cli
+    __all__ += ['AsciiDocAPI', 'execute', 'cli']
