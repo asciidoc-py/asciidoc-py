5beda97888fafc1be9943b8870b32da663784836
Matthew Peveler <matt.peveler@gmail.com>
2022-05-22 10:58:55 -0400

    Update __init__.py

--- asciidoc/__init__.py
+++ asciidoc/__init__.py
@@ -35,29 +35,6 @@ def get_compat_mode() -> int:
     return COMPAT_MODE
 
 
-COMPAT_MODE = 1
-
-
-def set_legacy_compat() -> None:
-    set_compat_mode(1)
-
-
-def set_future_compat() -> None:
-    set_compat_mode(2)
-
-
-def set_compat_mode(mode: int) -> None:
-    if mode < 1 or mode > 2:
-        raise ValueError('compat mode must be 1 <= mode <= 2')
-
-    global COMPAT_MODE
-    COMPAT_MODE = mode
-
-
-def get_compat_mode() -> int:
-    return COMPAT_MODE
-
-
 # If running as a script, we avoid these imports to avoid a circular
 # RuntimeWarning, which is fine as we don't use them in that case.
 if "-m" not in sys.argv:
