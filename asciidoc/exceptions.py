f906447c3e06b8c7e03398993b7eace430bee4dd
Erik <fallbackerik@gmail.com>
2024-07-06 16:50:42 +0200

    Update exceptions.py

--- asciidoc/exceptions.py
+++ asciidoc/exceptions.py
@@ -6,3 +6,8 @@ class EAsciiDoc(Exception):
 class AsciiDocError(Exception):
     """Exceptions raised by the asciidoc API"""
     pass
+
+class OnlyBookLvl0Sections(AsciiDocError):
+    """ if a level 0 section is discovered and the doctype is not book
+    """
+    pass
