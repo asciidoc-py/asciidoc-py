617ae123ac261b8f688c01815e181f42d275c181
Erik <fallbackerik@gmail.com>
2024-07-13 10:51:16 +0200

    Update asciidoc.py

--- asciidoc/asciidoc.py
+++ asciidoc/asciidoc.py
@@ -4245,24 +4245,9 @@ class Reader(Reader1):
 
     def __repr__(self):
         return "class Reader({})".format(json.dumps({
-            "f": str(self.f),
             "fname": self.fname,
-            "next": self.next,
             "cursor": self.cursor,
-            "tabsize": self.tabsize,
-            "parent": self.parent,
-            "_lineno": self._lineno,
-            "line_ranges": self.line_ranges,
-            "current_depth": self.current_depth,
-            "max_depth": self.max_depth,
-            "bom": self.bom,
-            "infile": self.infile,
-            "indir": self.indir,
-            "depth": self.depth,
-            "skip": self.skip,
-            "skipname": self.skipname,
-            "skipto": self.skipto,
-        }, indent=2))
+        }))
 
     def read_super(self):
         result = Reader1.read(self, self.skip)
