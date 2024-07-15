503534ab0a9bec29e89ae262df482c1dbf38f1ba
Erik <fallbackerik@gmail.com>
2024-07-15 19:05:50 +0200

    Update asciidoc.py

--- asciidoc/asciidoc.py
+++ asciidoc/asciidoc.py
@@ -2107,14 +2107,9 @@ class AbstractBlock:
         self.mo = None
 
     def __repr__(self):
-        match = self.mo
-        print(match, match.string, match.start(), match.end())
-        print(str(match))
-        sys.stdout.flush()
-        match_string = match.string[:match.start] + "[" + match.string[match.start:match.end] + "]" + match.string[m.end:]
         return "class AbstractBlock({})".format(json.dumps({
             "defname": self.defname,
-            "match": match_string,
+            "match": str(self.mo),
         }, indent=2))
 
     @staticmethod
