804dc838bb179ddddbaad8f0798ed481aca554e9
Erik <fallbackerik+v2@gmail.com>
2024-07-19 10:44:33 +0200

    Add new Exception type about heading lvl filtering

--- asciidoc/exceptions.py
+++ asciidoc/exceptions.py
@@ -2,6 +2,10 @@ class EAsciiDoc(Exception):
     """Exceptions raised by the main asciidoc process"""
     pass
 
+class EOnlyBookLvl0Sections(EAsciiDoc):
+    """ Parser only allows lvl 0 headings on doctype book
+    """
+    pass
 
 class AsciiDocError(Exception):
     """Exceptions raised by the asciidoc API"""
